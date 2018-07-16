7e8deaf69c85fed4968ac55bbfc4612ec5ec5862
Admin <mercerbe@gmail.com>
2018-07-16 15:38:26 -0400

    good test on page scrape. todo: reformat for request, ditch axios...

--- routes/scrapeRoutes.js
+++ routes/scrapeRoutes.js
@@ -1,5 +1,6 @@
 //============================dependencies=====================================//
 const db = require('../models')
+const request = require('request')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const bodyParser = require('body-parser')
@@ -7,50 +8,60 @@ const bodyParser = require('body-parser')
 //=============================scraping routes==================================//
 module.exports = app => {
 
-  //get route to scrape website
-  app.get('/scrape', (req, res) => {
-
-    //use axios to grab html
-    //option to switch to cryptopanic.com
-    axios.get("https://www.the-blockchain.com/category/news/").then( response => {
-
-      //load html into  cheerio
-      let $ = cheerio.load(response.data)
-
-      //grab news elements
-      $("div.td-module-1").each( (i, element) => {
-
-        //object for each result
-        let result = {}
-
-        //grab children elements from div
-        result.headline = $(this)
-          .children("h3")
-          .text()
-          console.log("headline" + result.headline);
-        result.url = $(this)
-          .children("a")
-          .attr('href')
-        result.author = $(this)
-          .children(".td-post-author-name")
-          .text()
-        result.postedAt = $(this)
-          .children(".td-post-date")
-          .text()
-
-        //for each result, create new Article in DB
-        db.Article.create(result)
-          .then( dbArticle => {
-            console.log(dbArticle)
-          })
-          .catch( err => {
-            return res.json(err)
-          })
-      })
-
-      //after scrape, send message
-      res.send("Scrape Complete!")
+  //test cheerio
+  app.get('/test', (req, res) => {
+    request("https://www.the-blockchain.com/category/news/", (err, response, html) => {
+      if(!err) {
+        let $ = cheerio.load(html)
+        res.send(html)
+      }
     })
   })
+
+  // //get route to scrape website
+  // app.get('/scrape', (req, res) => {
+  //
+  //   //use axios to grab html
+  //   //option to switch to cryptopanic.com
+  //   request("https://www.the-blockchain.com/category/news/").then( response => {
+  //
+  //     //load html into  cheerio
+  //     let $ = cheerio.load(response.data)
+  //
+  //     //grab news elements
+  //     $("div.td-module-1").each( (i, element) => {
+  //
+  //       //object for each result
+  //       let result = {}
+  //
+  //       //grab children elements from div
+  //       result.headline = $(this)
+  //         .children("h3")
+  //         .text()
+  //         console.log("headline" + result.headline);
+  //       result.url = $(this)
+  //         .children("a")
+  //         .attr('href')
+  //       result.author = $(this)
+  //         .children(".td-post-author-name")
+  //         .text()
+  //       result.postedAt = $(this)
+  //         .children(".td-post-date")
+  //         .text()
+  //
+  //       //for each result, create new Article in DB
+  //       db.Article.create(result)
+  //         .then( dbArticle => {
+  //           console.log(dbArticle)
+  //         })
+  //         .catch( err => {
+  //           return res.json(err)
+  //         })
+  //     })
+  //
+  //     //after scrape, send message
+  //     res.send("Scrape Complete!")
+  //   })
+  // })
 
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const db = require('./models')
-
+const request = require('request')
 //express setup
 const app = express()
 const PORT = process.env.PORT || 8080
